2bc51f5ce49f9d840e6c96fca5ff5159a461f605
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-05-17 02:07:00 +0300

    vm: создание вьюмодели и регистрация ее в контейнере

---
+++ DevTricks.ViewModels/RegistrationModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using DevTricks.ViewModels.MainWindow;
+
+namespace DevTricks.ViewModels
+{
+    public class RegistrationModule : Module
+    {
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            // Контейнер для регистрации
+            builder.RegisterType<MainWindowViewModel>()     // - регистрируемый тип
+                .As<IMainWindowViewModel>()                 // - контрак вьюмодели (интерфейс, под которым регистрируем тип)
+                .InstancePerDependency();                   // - время жизни вьюмодели (для каждого запроса будет создаваться новый экземпляр вьюмодели)
+        }
+    }
+}
