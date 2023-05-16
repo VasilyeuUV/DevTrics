567505e127f7f848c2ff70e6ab7a7aa9495c0f15
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-05-17 01:13:19 +0300

    sln: перенос главного окна во View; bootstrapper: cоздание bootstrapper

---
+++ DevTricks.Bootstrapper/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using DevTricks.Views;
+using System.Windows;
+
+namespace DevTricks.Bootstrapper
+{
+    public class Bootstrapper : IDisposable
+    {
+
+        /// <summary>
+        /// Создание главного окна
+        /// </summary>
+        /// <returns></returns>
+        public Window Run()
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            return mainWindow;
+        }
+
+
+        //#################################################################################################################
+        #region IDisposable
+
+        public void Dispose()
+        {
+
+        }
+
+        #endregion // IDisposable
+    }
+}
