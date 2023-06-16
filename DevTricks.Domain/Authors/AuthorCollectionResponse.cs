cfc13b7bedff5e5429ebd63297f864b7688b09a2
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-06-16 15:52:47 +0300

    domain: author models

---
+++ DevTricks.Domain/Authors/AuthorCollectionResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace DevTricks.Domain.Authors
+{
+    /// <summary>
+    /// Коллекции для Авторов, полцчаемых с API
+    /// </summary>
+    public class AuthorCollectionResponse
+    {
+        /// <summary>
+        /// Список авторов, получаемых с API
+        /// </summary>
+        public required IEnumerable<AuthorModelResponse> Items { get; init; }
+    }
+}
---
+++ DevTricks.Domain/Authors/AuthorModelResponse.cs
@@ -0,0 +1,32 @@
+﻿namespace DevTricks.Domain.Authors
+{
+    /// <summary>
+    /// Модель Автора, получаемая из API
+    /// </summary>
+    public class AuthorModelResponse
+    {
+        /// <summary>
+        /// Идентификатор Автора
+        /// </summary>
+        public required int Id { get; init; }
+
+
+        /// <summary>
+        /// Имя Автора
+        /// </summary>
+        public required string FirstName { get; init; }
+
+
+        /// <summary>
+        /// Фамилия Автора
+        /// </summary>
+        public required string LastName { get; init; }
+
+
+        /// <summary>
+        /// Год рождения Автора
+        /// </summary>
+        public required DateOnly Birthdate{ get; init; }
+
+    }
+}
